f80e3a00079e0d8dcf59538c9a2f1db800751460
Raghav-Pal <automation.devops@gmail.com>
2020-07-06 19:03:52 +0530

    updated from mac - Applitools tests

---
+++ Object Repository/Page_Applitools/div_Hello world                            _91be81.rs
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Hello world                            _91be81</name>
+   <tag></tag>
+   <elementGuidId>3d3f0ae0-a509-4f5e-afd7-e3f450cb78bd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>demo-page center</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+            Hello world!
+        
+        
+            Hello world!
+        
+    
+
+    
+        Welcome to your first Applitools Eyes test!!!
+
+        Add ?diff1 to the end of the url to make the time field below dynamic (time)
+            random.
+        Add ?diff2 to the end of the url to simulate additional differences.
+
+        
+            HH:MM:SS.sss
+            16:3:18.471
+        
+    
+
+    
+        Enter Your Name : 
+    
+    
+        Click me!
+    
+
+    
+         PLEASE ENTER YOUR NAME
+
+        
+        
+        
+    
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;demo-page center&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div</value>
+   </webElementXpaths>
+</WebElementEntity>
